8b20ee1214722f3b3f2bb0dacb98394dcc8410b1
nishimura55 <naoki.hnmr55@gmail.com>
2021-03-28 21:55:33 +0900

    TaskAreaコンポーネントに切り分け

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchTask } from '../actions';
+import React from 'react';
+import { TaskArea } from './TaskArea';
 
-export const App = () => {
-  const tasks = useSelector(state => state.tasks);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchTask());
-  }, [])
-
-  return (
-    <ul>
-      {tasks.length && tasks.map(task => (
-        <li key={task.id}>{task.name}</li>
-      ))}
-    </ul>
-  );
-}
+export const App = () =>
+  (
+    <>
+      <TaskArea/>
+    </>
+  )
---
+++ frontend/src/components/TaskArea.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchTask } from '../actions';
+
+export const TaskArea = () => {
+  const tasks = useSelector(state => state.tasks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTask());
+  }, [])
+
+  return (
+    <ul>
+      {tasks.length && tasks.map(task => (
+        <li key={task.id}>{task.name}</li>
+      ))}
+    </ul>
+  );
+}
